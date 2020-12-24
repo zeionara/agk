f5e28fede301ab2ac6778c03f5a14b10b4a2ac6c
Dmitrii <zeionara@gmail.com>
2020-12-25 00:01:38 +0300

    Implemented a basic authentication filter

--- Sources/agk/cli/StartServer.swift
+++ Sources/agk/cli/StartServer.swift
@@ -1,11 +1,36 @@
 import ArgumentParser
 import PerfectHTTP
 import PerfectHTTPServer
+import Foundation
+
+public extension Array where Element == (String, String) {
+    subscript(index: String) -> String? {
+        for item in self {
+            if item.0 == index {
+                return item.1
+            }
+        }
+        return Optional.none
+    } 
+}
+
+private func encodeCredentials(login: String?, password: String?) -> String? {
+    if let login_ = login, let password_ = password {
+        return "\(login_):\(password_)".data(using: .utf8)!.base64EncodedString(options: Data.Base64EncodingOptions(rawValue: 0))
+    }
+    return Optional.none
+}
 
 struct StartServer: ParsableCommand {
 
     @Option(name: .shortAndLong, help: "Port for the server to listen to")
     private var port: Int = 1719
+
+    @Option(name: .shortAndLong, help: "Login for accessing the service")
+    private var login: String = ""
+
+    @Option(help: "Password for accessing the service")
+    private var password: String = ""
 
     func handler(request: HTTPRequest, response: HTTPResponse) {
         response.setHeader(.contentType, value: "text/html")
@@ -13,15 +38,37 @@ struct StartServer: ParsableCommand {
         response.completed()
     }
 
+    struct AuthenticationFilter: HTTPRequestFilter {
+        let targetCredentials: String?
+
+        func filter(request: HTTPRequest, response: HTTPResponse, callback: (HTTPRequestFilterResult) -> ()) {
+            if targetCredentials == Optional.none || request.header(.authorization)?.components(separatedBy: " ")[1] ?? "" == targetCredentials! {
+                callback(.continue(request, response))
+            } else {
+                response.status = .forbidden
+                callback(.halt(request, response))
+            }
+        }
+    }
+
     mutating func run() throws {
         print("Starting an http server...")
+        
         var routes = Routes()
         routes.add(method: .get, uri: "/", handler: handler)
-        // routes.add(method: .get, uri: "/**", handler: StaticFileHandler(documentRoot: "./webroot", allowResponseFilters: true).handleRequest)
+        
         try HTTPServer.launch(
             name: "localhost",
             port: port,
             routes: routes,
+            requestFilters: [
+                (
+                    AuthenticationFilter(
+                        targetCredentials: encodeCredentials(login: login.isEmpty ? Optional.none : login, password: password.isEmpty ? Optional.none : password)
+                    ),
+                    HTTPFilterPriority.high
+                )
+            ],
             responseFilters: [
                 (
                     PerfectHTTPServer.HTTPFilter.contentCompression(data: [:]),
